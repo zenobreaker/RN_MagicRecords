0cc1ddeb652be97f733d6f1caf8a1c6372e75057
choi je seong <wpsh6369@gmail.com>
2024-10-30 02:37:13 +0900

    이동 기능 테스트

---
+++ Assets/6.Scripts/PlayerMovingComponent.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovingComponent : MonoBehaviour
+{
+    /// <summary>
+    /// Input Keyboard
+    /// </summary>
+    [Header("Input Settings")]
+    private Vector2 inputMove;
+    public Vector2 InputMove { get => inputMove; }
+
+    [SerializeField] private float sensitivity = 10.0f;     // 입력 감도
+    [SerializeField] private float deadZone = 0.001f;       // 대시 등의 감소시간
+
+    private Vector2 currentInputMove; // 현재 입력한 이동값
+    private Vector2 velocity;
+    private Quaternion rotation;
+
+
+    [Header("Speed")]
+    [SerializeField] private SO_Movement SO_Movement;
+    private SO_Movement movement;
+    /// <summary>
+    /// Run & Sprint 
+    /// </summary>
+    private bool bRun = false;
+
+
+    #region COMPONENT PROPERTY
+
+    private Animator animator;
+
+    #endregion
+
+    private bool bCanMove = true;
+
+    #region PUBLIC MOVE & STOP 
+    public void Move()
+    {
+        bCanMove = true;
+    }
+
+    public void Stop()
+    {
+        bCanMove = false;
+    }
+    #endregion
+
+    private void Awake()
+    {
+        movement = SO_Movement.GetMovement();
+
+        Awake_PlayerBindInput();
+    }
+
+    private void Awake_PlayerBindInput()
+    {
+        PlayerInput input = GetComponent<PlayerInput>();
+        Debug.Assert(input != null);
+        InputActionMap actionMap = input.actions.FindActionMap("Player");
+        Debug.Assert(actionMap != null);
+
+        InputAction moveAction = actionMap.FindAction("Move");
+        Debug.Assert(moveAction != null);
+
+        moveAction.performed += Input_Move_Performed;
+        moveAction.canceled += Input_Move_Canceled;
+    }
+
+
+    private void Input_Move_Performed(InputAction.CallbackContext context)
+    {
+        inputMove = context.ReadValue<Vector2>();
+    }
+    private void Input_Move_Canceled(InputAction.CallbackContext context)
+    {
+        inputMove = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        Debug.Log(inputMove);
+        currentInputMove = Vector2.SmoothDamp(currentInputMove, inputMove, ref velocity, 1.0f / sensitivity);
+
+        //TODO : Condition에 따라서 이동 불가 
+
+        if (bCanMove == false)
+            return;
+
+        float speed = bRun ? movement.RunSpeed : movement.WalkSpeed;
+
+        Vector3 direction = Vector3.zero;
+        if (currentInputMove.magnitude > deadZone)
+        {
+            direction = (Vector3.right * currentInputMove.x) + (Vector3.forward * currentInputMove.y);
+            direction.y = 0;
+
+            transform.localRotation = Quaternion.LookRotation(direction);
+        }
+
+        direction = direction.normalized * speed;
+        transform.Translate(direction * Time.deltaTime, Space.World);
+
+        //TODO: animator에 속도 추가
+
+    }
+
+
+
+
+
+}
---
+++ Assets/6.Scripts/SO_Movement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_Movement", menuName = "Scriptable Objects/SO_Movement")]
+public class SO_Movement : ScriptableObject
+{
+    public enum SpeedType
+    {
+        Walk = 1,
+        Run = 2,
+        Sprint = 3,
+    };
+
+
+    [Header("Speed")]
+    [SerializeField] private float walkSpeed = 2.0f;
+    [SerializeField] private float runSpeed = 4.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
+
+    public float WalkSpeed { get => walkSpeed; }
+    public float RunSpeed { get => runSpeed; }
+    public float SprintSpeed { get => SprintSpeed; }
+
+
+    public SO_Movement GetMovement()
+    {
+        SO_Movement movement = SO_Movement.CreateInstance<SO_Movement>();
+        movement.walkSpeed = walkSpeed;
+        movement.runSpeed = runSpeed;
+        movement.sprintSpeed = sprintSpeed;
+        return movement;
+    }
+}
