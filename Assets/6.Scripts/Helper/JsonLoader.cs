5ae306bd9c959434e174dd27b32a2d4bd874bf91
choi je seong <wpsh6369@gmail.com>
2025-07-26 05:45:25 +0900

    Helper Json Loading 기능 클래스 추가

---
+++ Assets/6.Scripts/Helper/JsonLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class JsonLoader
+{
+    public static void LoadJsonList<TJsonRoot, TJsonData, TResult>(
+        TextAsset jsonText,
+        Func<TJsonRoot, List<TJsonData>> extractListFunc,
+        Func<TJsonData, TResult> converFunc,
+        Action<TResult> onResult)
+    {
+        var rootData = JsonUtility.FromJson<TJsonRoot>(jsonText.text);
+        if (rootData == null)
+        {
+            Debug.LogError("JsonLoader Create Fail rootData");
+            return;
+        }
+
+        var dataList = extractListFunc(rootData);
+        if (dataList == null) return; 
+
+        foreach(var jsonData in dataList)
+        {
+            var result = converFunc(jsonData);
+            onResult?.Invoke(result);
+        }
+    }
+
+    public static List<int> ParseIntList(string csv)
+    {
+        return string.IsNullOrEmpty(csv) == false ? csv.Split(',')
+            .Select(str => int.Parse(str.Trim()))
+            .ToList() : new List<int>();
+    }
+}
+
