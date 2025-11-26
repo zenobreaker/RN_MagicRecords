d36cb4e6b2014543e1ac0d6d67cf554a98db8400
choi je seong <wpsh6369@gmail.com>
2025-11-27 08:39:06 +0900

    Effect 관리용 매니저 추가

---
+++ Assets/6.Scripts/Managers/EffectManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 여러 효과를 발동시키는 매니저 
+/// </summary>
+public class EffectManager : Singleton<EffectManager>
+{
+    private Dictionary<GameObject, EffectComponent> activeEffects = new();
+
+    public void RegisterEffect(GameObject owner, GameObject appliedBy, BaseEffect effect)
+    {
+        if (!activeEffects.ContainsKey(owner))
+        {
+            var buff = owner.GetComponent<EffectComponent>();
+            activeEffects[owner] = buff;
+
+            if (owner.TryGetComponent<Character>(out var character))
+            {
+                character.OnDead += UnregisterAllEffects;
+            }
+        }
+        
+        activeEffects[owner]?.ApplyEffect(effect, owner, appliedBy);
+    }
+
+    public void UnregisterEffect(Character target, BaseEffect effect)
+    {
+        if (!activeEffects.ContainsKey(target)) return; 
+
+        activeEffects[target].RemoveEffect(effect);
+       // effect.Remove(target);
+    }
+
+    public void UnregisterAllEffects(Character target)
+    {
+        if (activeEffects.ContainsKey(target))
+        {
+            activeEffects.Remove(target);
+        }
+    }
+}
