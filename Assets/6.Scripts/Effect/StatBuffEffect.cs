b9765fb7ee322921ab157d48adfae29e48ab1abf
choi je seong <wpsh6369@gmail.com>
2025-11-30 08:48:26 +0900

    스탯버프이펙트 내용 수정

--- Assets/6.Scripts/Effect/StatBuffEffect.cs
+++ Assets/6.Scripts/Effect/StatBuffEffect.cs
@@ -9,6 +9,15 @@ public class StatBuffEffect
     private StatModifier modifier;
     private StatusComponent status;
 
+    public StatBuffEffect(string id, float duration, StatModifier modifier, ModifierValueType type)
+       : base(id, "Stat Buff", duration)
+    {
+        this.modifier = modifier;
+
+        // 스탯 적용은 Apply에서만 한다.
+        Actions.Add(new ApplyStatModifierAction(modifier));
+    }
+
     public StatBuffEffect(string id, float duration, StatusType type,
         float amount, ModifierValueType valueType = ModifierValueType.PERCENT)
         : base(id, "Stat Buff", duration)
