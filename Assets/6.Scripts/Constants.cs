f78e70a1b3e58234702a0388e05be5c226681a36
choi je seong <wpsh6369@gmail.com>
2025-12-08 19:28:44 +0900

    상수 값 추가

--- Assets/6.Scripts/Constants.cs
+++ Assets/6.Scripts/Constants.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public static class Constants
 {
     public const int ENHANCE_MAX_COUNT = 15;
+
+    public const float DEFAULT_CIRT_DMG_RATIO = 1.5f;
+
     public const string BLEED_DMG_COLOR = "8A0A0A";
     public const string BURN_DMG_COLOR = "F65912";
     public const string POISON_DMG_COLOR = "588200";
