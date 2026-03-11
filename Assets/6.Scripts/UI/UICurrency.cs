1ef0d53f7ac074ff2e2baf14069cb883f1c226f0
choi je seong <wpsh6369@gmail.com>
2026-03-11 22:03:12 +0900

    재화 그리는 UI 클래스 로직 수정

--- Assets/6.Scripts/UI/UICurrency.cs
+++ Assets/6.Scripts/UI/UICurrency.cs
@@ -1,4 +1,4 @@
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,20 +8,28 @@ public class UICurrency : MonoBehaviour
     [SerializeField] private Image currencyIcon;
     [SerializeField] private TextMeshProUGUI currencyText;
 
+    [Header("Database")]
+    [SerializeField] private SO_CurrencyIconDatabase iconDatabase; // SO 연결 슬롯
+
     public CurrencyType Type => type; 
     private int currencyValue;
 
     private void Awake()
     {
-        currencyIcon = transform.FindChildByName("Icon").GetComponentInChildren<Image>();
-        currencyText = transform.FindChildByName("Cost").GetComponentInChildren<TextMeshProUGUI>();
+        // Null 레퍼런스 방지를 위해 Transform 캐싱 후 GetComponent 수행
+        Transform iconTf = transform.FindChildByName("Icon");
+        if (iconTf != null) currencyIcon = iconTf.GetComponentInChildren<Image>();
+
+        Transform costTf = transform.FindChildByName("Cost");
+        if (costTf != null) currencyText = costTf.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     private void Draw()
     {
         if(currencyIcon != null)
         {
-
+            // 타입에 맞는 아이콘을 SO에서 찾아 적용
+            currencyIcon.sprite = iconDatabase.GetIcon(type);
         }
 
 
