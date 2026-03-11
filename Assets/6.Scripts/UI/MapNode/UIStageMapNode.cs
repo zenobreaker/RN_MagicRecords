f3e96adc957b825c67f61ee7546a76cd8fda9b83
choi je seong <wpsh6369@gmail.com>
2026-03-11 22:01:39 +0900

    스테이지 노드 - 노드에 아이콘을 그리는 기능 추가

--- Assets/6.Scripts/UI/MapNode/UIStageMapNode.cs
+++ Assets/6.Scripts/UI/MapNode/UIStageMapNode.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIStageMapNode 
     : UIMapNode
 {
     [SerializeField] private StageInfo stageInfo;
+    [SerializeField] private Image stageIcon; 
 
     public event Action<StageInfo> OnClicked;
 
@@ -14,9 +16,24 @@ public class UIStageMapNode
         base.Init(mapNode);
 
         stageInfo = AppManager.Instance.GetStageInfoMatchedMapNode(mapNode);
+
+        DrawStageIcon();
     }
     public void OnClick()
     {
         OnClicked?.Invoke(stageInfo);
+    }
+
+    private void DrawStageIcon()
+    {
+        if(stageInfo == null || stageIcon == null) return;
+
+        Sprite icon = AppManager.Instance.GetStageIcon(stageInfo.type);
+
+        if (icon != null)
+        {
+            stageIcon.sprite = icon;
+            stageIcon.gameObject.SetActive(true);
+        }
     }
 }
