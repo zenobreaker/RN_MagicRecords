d842bc895a95582e964a6b9f39e28759e5833222
choi je seong <wpsh6369@gmail.com>
2026-07-29 23:46:10 +0900

    오류 수정

--- Assets/6.Scripts/Managers/PlayerManager.cs
+++ Assets/6.Scripts/Managers/PlayerManager.cs
@@ -197,15 +197,6 @@ public class PlayerManager :
         return jobdata.GetJobInfo(jobID);
     }
 
-    public List<JobInfo> GetAvailableJobs(int charId)
-    {
-        List<JobInfo> result = new List<JobInfo>();
-
-
-
-        return result;
-    }
-
     public void SaveIfDirty()
     {
         if (isDirty == false) return;
